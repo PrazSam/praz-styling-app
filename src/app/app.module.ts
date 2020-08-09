0d7b7a2713a0234f542b1a4850650c77e1b86e87
PrazSam <prasanna.samara.91@gmail.com>
2020-08-09 21:08:17 +0530

    2 files modified, 8 added, 6 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,15 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { AppRoutingModule } from './app.routing.module';
-import { HtmlEntitiesAndSemanticsComponent } from './html-entities-and-semantics/html-entities-and-semantics.component';
-import { CssBasicsComponent } from './css-basics/css-basics.component';
+import { HtmlEntitiesAndSemanticsComponent } from './a-html-entities-and-semantics/html-entities-and-semantics.component';
+import { CssBasicsComponent } from './b-css-basics/css-basics.component';
 import { DBoxModelComponent } from './d-box-model/d-box-model.component';
 import { CBackgroundAndBoardersComponent } from './c-background-and-boarders/c-background-and-boarders.component';
+import { ESideMenuComponent } from './e-side-menu/e-side-menu.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, AppRoutingModule ],
-  declarations: [ AppComponent, HelloComponent, HtmlEntitiesAndSemanticsComponent, CssBasicsComponent, DBoxModelComponent, CBackgroundAndBoardersComponent ],
+  declarations: [ AppComponent, HelloComponent, HtmlEntitiesAndSemanticsComponent, CssBasicsComponent, DBoxModelComponent, CBackgroundAndBoardersComponent, ESideMenuComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { CssBasicsComponent } from './css-basics/css-basics.component';
+import { CssBasicsComponent } from './b-css-basics/css-basics.component';
 import { DBoxModelComponent } from './d-box-model/d-box-model.component';
-import { HtmlEntitiesAndSemanticsComponent } from './html-entities-and-semantics/html-entities-and-semantics.component';
+import { HtmlEntitiesAndSemanticsComponent } from './a-html-entities-and-semantics/html-entities-and-semantics.component';
 
 const appRoutes: Routes = [
   { 
